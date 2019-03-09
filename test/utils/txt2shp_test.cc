f293ef81bebb5aa15fbce54d15726577e17ff183
liuxinhhui <2201611731@qq.com>
2019-03-09 15:13:40 +0800

    test

--- test/utils/txt2shp_test.cc
+++ test/utils/txt2shp_test.cc
@@ -2,14 +2,22 @@
  * \file shp_helper_test.cc
  * \desc The shp helper test unit
  */
-#include "mod/utils/txt2shp.h"
+#include "/mod/utils/txt2shp.h"
+//#include "/mod/storage/road_network.h"
+#include "/mod/storage/trajectory.h"
 
-#include "gtest/gtest.h"
+
+#include "/gtest/gtest.h"
 
 namespace mod {
 
 TEST(TestShpHelper, TestAll) {
 
+}
+int main(){
+    RoadNetwork("data/WA/WA_Edges.txt","data/WA/WA_Nodes.txt");
+    Trajectory("data/traj/input_01.txt");
+    return 0;
 }
 
 }  // namespace mod
