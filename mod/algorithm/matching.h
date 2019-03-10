e35fb58e64347b07081d0a9f2ca82066b3d0b92c
liuxinhhui <2201611731@qq.com>
2019-03-10 19:22:28 +0800

    algotrithm/2019/3/10

--- mod/algorithm/matching.h
+++ mod/algorithm/matching.h
@@ -2,3 +2,31 @@
  * \file matching.h
  * \desc The trajectory road-network matching algorithm.
  */
+
+#pragma once
+
+ #include "mod/algorithm/index.h"
+
+ #include <vector>
+ namespace mod{
+    //匹配结果
+     struct Result{
+        struct TrajectoryNode t_point;
+        struct Edge t_edge;
+      }
+      //结果集
+      std::vector<struct Result> matching_result;
+
+      //匹配准确度
+      //（int 数组）???
+      float accuracy;
+
+      class Matching{
+          public:
+              Matching();
+              //匹配轨迹点到边缘上
+              std::vector<struct Result> getMatchingResult(std::vector<struct TrajectoryNode> trajectory,struct Edge *edge,int index);
+              //计算准确度
+              float accuracy(std::vector<struct Result> matching_result,std::vector<struct Result> truth_result);
+          };
+ }
--- mod/algorithm/passby_matching.h
+++ mod/algorithm/passby_matching.h
@@ -2,3 +2,28 @@
  * \file passby_matching.h
  * \desc The passby trajectory and road-network matching algorithm. 
  */
+
+#pragma once
+
+ #include "mod/algorithm/index.h"
+ #include "mod/algorithm/matching.h"
+
+ #include <vector>
+//#常量还没定义
+ namespace mod{
+
+     class PassbyMatching{
+        public:
+            //返回点到轨迹片段的距离
+        	double minDist(Point point ,struct SegTrajectory seg);
+        	//返回所有距离点严格小于阈值的所有Edgey*
+            vector<struct Edge*> getNearEdges(Point point,double threshold);
+            //匹配距离点最近的Edge*
+            struct Edge* getNearestEdge(Point point,double &shortestDist);
+        	//初始化?轨迹段索引
+        	Point init(std::vector<struct TrajectoryNode> trajectory,Point point,int index)
+            //传递概率
+            int pass(std::vector<struct TrajectoryNode> trajectory,Point point,int index);
+
+     }
+ }
