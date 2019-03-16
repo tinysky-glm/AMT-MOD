8d2b0e17642f237e97e19da1cb71900f1386b5b6
liuxinhhui <2201611731@qq.com>
2019-03-16 09:47:40 +0800

    storage/2019/3/16

--- mod/storage/road_network.cc
+++ mod/storage/road_network.cc
@@ -1,5 +1,9 @@
 /*!
-* \file road_network.cc
+
+
+
+
+*\file road_network.cc
 * \brief The road network
 */
 #include "mod/storage/road_network.h"
@@ -14,55 +18,51 @@ using namespace mod;
 RoadNetwork::RoadNetwork(const std::string& node_file,const std::string& edge_file){
 
     std:: ifstream infile;
-    int index = 0;
     std::string temp;
+    //顶点
+    infile.open(node_file.c_str());
+    if (!infile.is_open()){
+        std:: cout << "未成功打开文件" << std::endl;
+    }
+    while(getline(infile,temp)) {
+        float f[3]={0.0};
+        std::istringstream iss;
+        iss.str(temp);
+        std::string s;
+        int i=0;
+        while (iss >> s){
+            f[i] = atof(s.c_str());
+            i++;
+        }
+      Point p(f[1], f[2]);
+      nodes_.push_back(p);
+    }
+
+
     //读取交叉口点
+    int index = 0;
+    //邻接矩阵初始化
+    for(int i=0;i < nodes_.size();i++) 
+    {
+        std::vector<mod_int_t> vec(nodes_.size(),0);
+        link_lists_.push_back(vec);
+    }
     infile.open(edge_file.c_str());  //infile按照指定路径读取txt文件
     if (!infile.is_open()){
        std::cout << "未成功打开文件" <<std::endl;
        exit(EXIT_FAILURE);
      }
-     while(getline(infile,temp)) {
-         int a[4]={0};//文件中4个数值
-         std::stringstream iss;//istringstream读 string
-         iss.str(temp);//temp 复制给 iss
-         std::string s;
-         int i=0;
-         while (iss >> s){
-             a[i] = stoi(s.c_str());
-             i++;
-         }
-       link_lists_[a[1]][a[2]] = a[3];
-     }
-
-     //顶点
-     //const int NODE_LENGTH = 600000;
-     infile.open(node_file.c_str());
-     if (!infile.is_open()){
-          std:: cout << "未成功打开文件" << std::endl;
-      }
-     char c;
-     while(infile.get(c))
-     {
-          if(c='\n')
-             index++;
-     }
-     std::cout << "*******index*******=****" << index <<std::endl;
-     nodes_.resize(index+1);
-     while(getline(infile,temp)) {
-         // Node p;
-          float f[3]={0.0};
-          std:: istringstream iss;
-          iss.str(temp);
-          std::string s;
-          int i=0;
-          while (iss >> s){
-              f[i] = atof(s.c_str());
-              i++;
-          }
-        Point p(f[1], f[2]);
-        nodes_[f[0]] = p;
-      }
-
+    while(getline(infile,temp)) {
+        int a[4]={0};//文件中4个数值
+        std::stringstream iss;//istringstream读 string
+        iss.str(temp);//temp 复制给 iss
+        std::string s;
+        int i=0;
+        while (iss >> s){
+            a[i] = atoi(s.c_str());
+            i++;
+        } 
+        link_lists_[a[1]][a[2]]=1;
+        link_lists_[a[2]][a[1]]=1;
+    }
 }
-
--- mod/storage/trajectory.cc
+++ mod/storage/trajectory.cc
@@ -13,37 +13,23 @@ using  namespace mod;
 Trajectory::Trajectory(const std::string& file){
     std::ifstream infile;
     std::string temp;
-    //const int POINTS_LENGTH = 3000;
     int index = 0;
     infile.open(file.c_str());
     if (!infile.is_open()){
         std::cout << "未成功打开文件" << std::endl;
         exit(EXIT_FAILURE);
      }
-     char c;
-     while(infile.get(c))
-     {
-          if(c='\n')
-             index++;
-     }
-    // std::cout << "*******index*******=****" << index <<std::endl;
-    std::cout << "*******index*******=****" << index <<std::endl;
-    points_.resize(index+1);
     while(getline(infile,temp, ',')) {
-         //Point p = {0,0};
-         float a[2]={0.0};
+         float a[3]={0.0};
          std::istringstream iss;
          iss.str(temp);
          std::string s;
          int i=0;
          while (iss >> s){
-           // a[i]=aotf(s.c_str());
-            a[i] = stoi(s);
+            a[i]=atof(s.c_str());
             i++;
          }
 	 Point p(a[1], a[2]);
-         //p.lat = a[1];
-         //p.lon = a[2];
-         points_[a[0]] = p;
+         points_.push_back(p);
      }
 }
