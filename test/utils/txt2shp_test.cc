793059e6eceed17a69d13dbf938dc948d575227c
liuxinhhui <2201611731@qq.com>
2019-05-08 16:47:10 +0800

    2019/5/8

--- test/utils/txt2shp_test.cc
+++ test/utils/txt2shp_test.cc
@@ -9,72 +9,36 @@
 #include "mod/algorithm/index.h"
 #include "mod/algorithm/matching.h"
 #include "mod/algorithm/passby_matching.h"
+#include "mod/algorithm/matching_accuracy.h"
 #include "gtest/gtest.h"
-
+#define K_VALUE 5
+#define THRESHOLD 500.0
 namespace mod {
 
 TEST(TestShpHelper, TestAll) {
   RoadNetwork road_net_work = RoadNetwork("../../data/WA/WA_Nodes.txt", "../../data/WA/WA_Edges.txt");
-
   Trajectory trajectory = Trajectory("../../data/traj/input_01.txt");
- // std::vector<struct MatchingResult> results = PassByMatching(road_net_work,trajectory);
- 
-  //Index下函数测试
+  //Index索引建立
   std::vector<Point> nodes_  =road_net_work.nodes();
   std::vector<Point> points_  =trajectory.points();
   Index n_index = Index(nodes_);
   Index p_index = Index(points_);
-  /**double s = 500.0;
-  Point t_point = p_index.Search(nodes_[467262],5,s);
-  printf("\n%d****%f*****%f",t_point.index,t_point.lon,t_point.lat);
-*/
-//matching下函数测试
-/**Matching m;
-int edge_id = m.getMatchingSeg(p_index,nodes_[43]);
-printf("matching下函数测试getEdge	%d",edge_id);*/
-//passBymatching下函数测试
-PassbyMatching pn;
-std::vector<struct MatchingResult> edges = pn.getPassByMatching(p_index,road_net_work);
-for(int i = 0;i<edges.size();i++){
-	printf("passBymatching下函数%d$$$$$$ %d",edges[i].p_index,edges[i].edge_id);
-	
-}
-
-
- //int edge_id = m.getMatchingSeg(n_index,points_[0]);
-
-/**
-  //验证道路网点的获取
-  std::vector<Point> nodes_  =road_net_work.nodes();
-  for(int j=0;j<nodes_.size();j++)
-  {
-     printf("nodeLon: %f\tnodeLat:%f\tnodeIndex:%d\n",nodes_[j].lon,nodes_[j].lat,nodes_[j].index);
+  double threshold = THRESHOLD;
+  //初始交叉口获取
+  Point n_start_point = n_index.Search(points_[0],K_VALUE,threshold);
+  //获取匹配结果集
+  PassbyMatching pn;
+  std::vector<struct MatchingResult> results = pn.getPassByMatching(p_index,road_net_work,n_start_point);
+  printf("ID\t道路边id\n");
+  for(int i = 0;i<results.size();i++){
+      printf("%d\t%d\n",results[i].p_index,results[i].edge_id);
   }
-*/
-
-/**
-//验证道路网邻接表
-for(int i =0;i < link_lists_.size();i++)
-{
-  std::vector<struct Connection> conns = road_net_work.link_list(i);
-  for(int j=0;j<conns.size();j++)//遍历出发点所引出的边
-  {
-     printf("from %d to %d,the edge_id is %d\n",i,conns[j].point_id,conns[j].edge_id);
-  }
-}
-*/
-  
-
-/**
-  //验证轨迹点的获取
-  std::vector<Point> points_  =trajectory.points();
-  for(int j=0;j<points_.size();j++)
-  {
-     printf("pointLon: %f\tpointLat:%f\tpointIndex:%d\n",points_[j].lon,points_[j].lat,points_[j].index);
-  }
-*/
+  //计算准确度
+  MatchingAccuracy m_accuracy;
+  double a_matching = m_accuracy.getMatchingAccuracy(results,"../../data/traj/output_02.txt");
+  printf("Passby算法匹配准确度为%f",a_matching);
 }
 
 
 
-}  // namespace mod
+}
